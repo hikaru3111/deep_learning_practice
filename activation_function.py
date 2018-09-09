46983a6e30b1e97cbe2be04431c2293a6fd43276
hikaru3111 <gopoke3111@gmail.com>
2018-09-09 16:24:49 +0900

    fix activation function

--- activation_function.py
+++ activation_function.py
@@ -2,11 +2,8 @@
 import numpy as np
 import matplotlib.pylab as plt
 
-def step_function(x)):
-    if x > 0:
-        return 1
-    else:
-        return 0
+def step_function(x):
+    return np.array(x >0, dtype=np.int)
 
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
@@ -15,20 +12,20 @@ def relu(x):
     return np.maximum(x,0)
 
 #test
-st_x = np.arrange(-5.0,5.0,0.1)
+st_x = np.arange(-5.0,5.0,0.1)
 st_y = step_function(st_x)
 plt.plot(st_x,st_y)
 plt.ylim(-0.1, 1.1)
 plt.show()
 
-sg_x = np.arrange(-5.0,5.0,0.1)
+sg_x = np.arange(-5.0,5.0,0.1)
 sg_y = sigmoid(sg_x)
 plt.plot(sg_x,sg_y)
 plt.ylim(-0.1, 1.1)
 plt.show()
 
-re_x = np.arrange(-0.5,0.5,0.1)
-re_y = relu(x)
+re_x = np.arange(-0.5,0.5,0.1)
+re_y = relu(re_x)
 plt.plot(re_x,re_y)
-plt.ylim(-0.1,1,1)
+plt.ylim(-0.1,0.5)
 plt.show()
